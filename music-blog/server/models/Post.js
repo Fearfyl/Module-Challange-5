3cc4e2cfd6fe9c0bb70320d2f52856d03cdaa86f
Fearfyl <wadebuchanan18@gmail.com>
2025-04-10 17:34:09 -0400

    Added post model

---
+++ music-blog/server/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const musicSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+    artist: {
+        type: String,
+        required: true,
+    },
+    album: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    releaseDate: {
+        type: Date,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Music', musicSchema);
+
+
+
+
